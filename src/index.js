76410c3fced4b3eb80acf29170e492c789e42348
Ваше Ім'я <karffffagen@gmail.com>
2023-09-01 14:53:18 +0300

    add home pages adeptiv

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './component/App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/portfolio'>
       <App />
     </BrowserRouter>
   </React.StrictMode>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,8 +11,10 @@ export default function Home() {
             <p className={css.frd}>Front-end Developer</p>
             <h1>Nazar</h1>
             <p className={css.lorem}>Do you need to create a website? When creating sites, I provide the same site as the layout. I also guarantee a perfect technical aspect so that everything works exactly as it should. If you are interested in my portfolio or experience - write to me!</p>
-            <NavLink to="/my-project"><button className={css.Hbutton}>My-project</button></NavLink>
-            <NavLink to="/contact"><button className={css.Hbutton}>Kontakt</button></NavLink>
+            <div className={css.divButtons}>
+              <NavLink to="/my-project"><button className={css.Hbutton}>My-project</button></NavLink>
+              <NavLink to="/contact"><button className={css.Hbutton}>Kontakt</button></NavLink>
+            </div>
         </div>
     </div>
   )
