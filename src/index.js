9867348a7340cd5ca2454192a6347336006ca617
Ваше Ім'я <karffffagen@gmail.com>
2023-12-04 17:44:44 +0200

    fixed code

--- src/index.js
+++ src/index.js
@@ -1,18 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {RouterProvider,createHashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import App from './component/App/App'
-
-const router = createHashRouter([
-  {
-    path: "/*",
-    element: <App />,
-  }
-]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router}>
-    </RouterProvider>
+    <BrowserRouter basename='/portfolio'>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 )
