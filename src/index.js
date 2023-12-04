9a971e93c38510cc6b808850bb1c1b3806894840
Ваше Ім'я <karffffagen@gmail.com>
2023-12-04 17:41:10 +0200

    fixed code in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import {BrowserRouter} from 'react-router-dom'
+import {BrowserRouter,createHashRouter} from 'react-router-dom'
 import App from './component/App/App'
+
+const router = createHashRouter([
+  {
+    path: "/*",
+    element: <App />,
+  }
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename='/portfolio'>
+    <BrowserRouter router={router}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
