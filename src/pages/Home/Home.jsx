eb90faad52861c94eb3067f78173b3d85586bccc
Ваше Ім'я <karffffagen@gmail.com>
2023-12-05 11:07:14 +0200

    FC

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,35 @@
-import React from 'react'
+import React from "react";
 import { NavLink } from "react-router-dom";
-import css from "./Home.module.css"
+import css from "./Home.module.css";
 export default function Home() {
   return (
     <div className={css.home}>
-        <div className={css.photo}>
-            <img src="https://cdn.pixabay.com/photo/2023/07/25/18/42/vector-graphic-8149677_1280.jpg" alt="My_Photo_In_Car" className={css.imageHome}/>
+      <div className={css.photo}>
+        <img
+          src="https://cdn.pixabay.com/photo/2023/07/25/18/42/vector-graphic-8149677_1280.jpg"
+          alt="My_Photo_In_Car"
+          className={css.imageHome}
+        />
+      </div>
+      <div>
+        <p className={css.frd}>Front-end Developer</p>
+        <h1 className={css.h1Home}>Nazar</h1>
+        <p className={css.lorem}>
+          Are you in need of a website? When I create websites, I ensure that
+          the final product matches the provided layout precisely. Additionally,
+          I guarantee impeccable technical functionality to ensure everything
+          operates as intended. If you're interested in viewing my portfolio or
+          learning more about my experience, feel free to reach out to me!
+        </p>
+        <div className={css.divButtons}>
+          <NavLink to="/my-project">
+            <button className={css.Hbutton}>My-project</button>
+          </NavLink>
+          <NavLink to="/contact" className={css.linkButton}>
+            <button className={css.Hbutton}>Contact</button>
+          </NavLink>
         </div>
-        <div>
-            <p className={css.frd}>Front-end Developer</p>
-            <h1 className={css.h1Home}>Nazar</h1>
-            <p className={css.lorem}>Do you need to create a website? When creating sites, I provide the same site as the layout. I also guarantee a perfect technical aspect so that everything works exactly as it should. If you are interested in my portfolio or experience - write to me!</p>
-            <div className={css.divButtons}>
-              <NavLink to="/my-project"><button className={css.Hbutton}>My-project</button></NavLink>
-              <NavLink to="/contact" className={css.linkButton}><button className={css.Hbutton}>Contact</button></NavLink>
-            </div>
-        </div>
+      </div>
     </div>
-  )
+  );
 }
