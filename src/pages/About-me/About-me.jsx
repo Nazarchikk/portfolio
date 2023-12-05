ff6fbb7e9b66c157213aa5ab98b6734d6ac84eec
Ваше Ім'я <karffffagen@gmail.com>
2023-12-05 11:04:53 +0200

    FC

--- src/pages/About-me/About-me.jsx
+++ src/pages/About-me/About-me.jsx
@@ -1,19 +1,22 @@
-import React from 'react'
-import css from "./About-me.module.css"
+import React from "react";
+import css from "./About-me.module.css";
 
 export default function AboutMe() {
   return (
     <div className={css.about}>
-        <div className={css.photo}>
-          <div className={css.imageAbout_me}>
-          </div>
-        </div>
-        <div>
-            <h1 className={css.hi}>Hi Guys</h1>
-            <p className={css.about_me}>My name is Nazar. I am 16 years old. I live in Kropyvnytskyi. I am a beginning Front-end Developer. I am learning from GoIt.
-                I am taking a Front-end Developer course. I have a couple of my projects and a couple with the team. If you want to see them, go to My-project
-            </p>
-        </div>
+      <div className={css.photo}>
+        <div className={css.imageAbout_me}></div>
+      </div>
+      <div>
+        <h1 className={css.hi}>Hi Guys</h1>
+        <p className={css.about_me}>
+          My name is Nazar, and I'm 16 years old. I reside in Kropyvnytskyi. I
+          am an aspiring front-end developer currently enrolled in the Front-End
+          Developer course at GoIt. I have worked on a few individual projects
+          and collaborated on some team projects. If you're interested in
+          viewing them, please check out the "My Projects" section.
+        </p>
+      </div>
     </div>
-  )
+  );
 }
